f8a6322f6cd8734208d9173c1316ccb8663c222e
Erlend Storsve <erlendstorsve@gmail.com>
2024-11-15 16:53:53 +0100

    use prettierd instead of prettier

--- lua/configs/conform.lua
+++ lua/configs/conform.lua
@@ -1,15 +1,15 @@
 local options = {
   formatters_by_ft = {
     lua = { "stylua" },
-    css = { "prettier" },
-    html = { "prettier" },
-    ts = { "prettier" },
-    vue = { "prettier" },
+    css = { "prettierd" },
+    html = { "prettierd" },
+    ts = { "prettierd" },
+    vue = { "prettierd" },
   },
 
   format_on_save = {
     -- These options will be passed to conform.format()
-    timeout_ms = 500,
+    timeout_ms = 2500,
     lsp_fallback = true,
   },
 }
