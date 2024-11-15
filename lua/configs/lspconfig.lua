1b6789f4f988933a172de9a34b8a023ca744ab94
Erlend Storsve <erlendstorsve@gmail.com>
2024-11-15 17:02:12 +0100

    scss lsp

--- lua/configs/lspconfig.lua
+++ lua/configs/lspconfig.lua
@@ -53,7 +53,8 @@ lspconfig.volar.setup {
 }
 
 -- servers with default config
-local servers = { "html", "cssls", "eslint", "docker_compose_language_service", "dockerls", "tailwindcss" }
+local servers =
+  { "html", "cssls", "eslint", "docker_compose_language_service", "dockerls", "tailwindcss", "somesass_ls" }
 
 for _, lsp in ipairs(servers) do
   lspconfig[lsp].setup {
